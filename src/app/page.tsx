6477975cdba301a9c6b173dff97314d57e5c1d4d
HoangBuiVietDuc28102006 <derek.b.hoang@gmail.com>
2026-05-02 22:05:09 +1000

    Update layout to make background global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import ScannerGridBackground from "@/components/backgrounds/ScannerGridBackground";
-
 export default function Home() {
-  return (
-    <main className="relative isolate min-h-screen overflow-hidden bg-black">
-      <ScannerGridBackground />
-    </main>
-  );
+  return <main className="min-h-screen" />;
 }
